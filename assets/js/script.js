7b148f594ea60ab163203a5966ac9a4b2f833a0f
Martin Bradbury <martinbradbury@btinternet.com>
2024-01-28 18:49:43 +0000

    correct type on get request.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 const API_KEY = "qD953aQcufNv7manAzoI8uNeyVI";
-const API_URL = 'https://ci-jshint.herokuapp.com/api';
-const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
-    const data = await response.json;
+    const data = await response.json();
 
     if (response.ok) {
         console.log(data);
     }
+
 }
