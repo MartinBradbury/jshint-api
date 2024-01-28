7f3d13b96e832ad41a8a6a016355d6590c33ffa9
Martin Bradbury <martinbradbury@btinternet.com>
2024-01-28 19:09:37 +0000

    added js to display expiry on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
 
+}
+
+function displayStatus(data) {
+
+    let heading ="API Key Status";
+    let results = `Your Key is valid until`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
 }
